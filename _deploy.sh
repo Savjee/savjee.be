e69a66ed266608969d0879e8a9f8445f787dc9fd
Xavier Decuyper <xavier.decuyper@gmail.com>
2013-01-05 21:28:23 +0100

    Updated deploy script to add cache headers to files.

--- _deploy.sh
+++ _deploy.sh
@@ -1,15 +1,26 @@
 # Run Jekyll
-echo "################## Running Jekyll ##################"
+echo "-> Running Jekyll"
 Jekyll
 
 # Upload to S3!
-echo -e "\n\n\n################## Uploading to S3 ##################"
+echo "\n\n-> Uploading to S3"
 
-#s3cmd sync --acl-public --exclude '.DS_Store' --exclude '*.png' --delete-removed --reduced-redundancy _site/ s3://s3.savjee.be
-#s3cmd sync --acl-public --exclude '.DS_Store' --add-header="Expires: Sat, 20 Nov 2286 18:46:39 GMT" --delete-removed --reduced-redundancy _site/assets/ s3://s3.savjee.be/assets/
 
-# Sync Assets first!
-s3cmd sync --acl-public --exclude '*.*' --include '*.png'  --include  '*.css' --include '*.js' --add-header="Expires: Sat, 20 Nov 2286 18:46:39 GMT" --delete-removed --reduced-redundancy _site/assets/ s3://s3.savjee.be/assets/
+# Sync media files first (Cache: expire in 10weeks)
+echo "\n--> Syncing media files..."
+s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header="Expires: Sat, 20 Nov 2020 18:46:39 GMT" --add-header="Cache-Control: max-age=6048000" --reduced-redundancy _site/ s3://s3.savjee.be/
+
+# Sync Javascript and CSS assets next (Cache: expire in 1 week)
+echo "\n--> Syncing .js and .css files..."
+s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header="Cache-Control: max-age=604800" --reduced-redundancy _site/ s3://s3.savjee.be/
+
+# Sync html files (Cache: 2 hours)
+echo "\n--> Syncing .html"
+s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header="Cache-Control: max-age=7200, must-revalidate" --reduced-redundancy _site/ s3://s3.savjee.be/
 
 # Sync everything else, but ignore the assets!
-s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' --delete-removed --reduced-redundancy _site/ s3://s3.savjee.be
+echo "\n--> Syncing everything else"
+s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' --reduced-redundancy _site/ s3://s3.savjee.be
+
+# Sync: delete removed
+s3cmd sync --delete-removed _site/ s3://s3.savjee.be

f5da157e3e5cbfde7cda59fa24286f5d71f793dc
Xavier Decuyper <xavier.decuyper@gmail.com>
2013-01-15 12:00:03 +0100

    We're now deploying straight to www.savjee.be

--- _deploy.sh
+++ _deploy.sh
@@ -8,19 +8,19 @@ echo "\n\n-> Uploading to S3"
 
 # Sync media files first (Cache: expire in 10weeks)
 echo "\n--> Syncing media files..."
-s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header="Expires: Sat, 20 Nov 2020 18:46:39 GMT" --add-header="Cache-Control: max-age=6048000" --reduced-redundancy _site/ s3://s3.savjee.be/
+s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header="Expires: Sat, 20 Nov 2020 18:46:39 GMT" --add-header="Cache-Control: max-age=6048000" --reduced-redundancy _site/ s3://www.savjee.be/
 
 # Sync Javascript and CSS assets next (Cache: expire in 1 week)
 echo "\n--> Syncing .js and .css files..."
-s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header="Cache-Control: max-age=604800" --reduced-redundancy _site/ s3://s3.savjee.be/
+s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header="Cache-Control: max-age=604800" --reduced-redundancy _site/ s3://www.savjee.be
 
 # Sync html files (Cache: 2 hours)
 echo "\n--> Syncing .html"
-s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header="Cache-Control: max-age=7200, must-revalidate" --reduced-redundancy _site/ s3://s3.savjee.be/
+s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header="Cache-Control: max-age=7200, must-revalidate" --reduced-redundancy _site/ s3://www.savjee.be
 
 # Sync everything else, but ignore the assets!
 echo "\n--> Syncing everything else"
-s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' --reduced-redundancy _site/ s3://s3.savjee.be
+s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' --reduced-redundancy _site/ s3://www.savjee.be/
 
 # Sync: remaining files & delete removed
-s3cmd sync --acl-public --delete-removed _site/ s3://s3.savjee.be
+s3cmd sync --acl-public --delete-removed --reduced-redundancy _site/ s3://www.savjee.be/

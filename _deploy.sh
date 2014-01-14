2b717984ec994ebc99add1ab72d7b04909a7e025
Xavier Decuyper <xavier.decuyper@gmail.com>
2014-01-14 12:54:58 +0100

    Changed _deploy script to use some configuration parameters

--- _deploy.sh
+++ _deploy.sh
@@ -1,26 +1,30 @@
+# Configuration options
+BUCKET='s3://gzip.savjee.be/'
+SITE_DIR='_site/'
+
 # Run Jekyll
-echo "-> Running Jekyll"
+echo '-> Running Jekyll'
 Jekyll build
 
 # Upload to S3!
-echo "\n\n-> Uploading to S3"
+echo '\n\n-> Uploading to S3'
 
 
 # Sync media files first (Cache: expire in 10weeks)
-echo "\n--> Syncing media files..."
-s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header="Expires: Sat, 20 Nov 2020 18:46:39 GMT" --add-header="Cache-Control: max-age=6048000"  _site/ s3://gzip.savjee.be/
+echo '\n--> Syncing media files...'
+s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header='Expires: Sat, 20 Nov 2020 18:46:39 GMT' --add-header='Cache-Control: max-age=6048000' $SITE_DIR $BUCKET
 
 # Sync Javascript and CSS assets next (Cache: expire in 1 week)
-echo "\n--> Syncing .js and .css files..."
-s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header="Cache-Control: max-age=604800"  _site/ s3://gzip.savjee.be
+echo '\n--> Syncing .js and .css files...'
+s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header='Cache-Control: max-age=604800' $SITE_DIR $BUCKET
 
 # Sync html files (Cache: 2 hours)
-echo "\n--> Syncing .html"
-s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header="Cache-Control: max-age=7200, must-revalidate"  _site/ s3://gzip.savjee.be
+echo '\n--> Syncing .html'
+s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header='Cache-Control: max-age=7200, must-revalidate' $SITE_DIR $BUCKET
 
 # Sync everything else, but ignore the assets!
-echo "\n--> Syncing everything else"
-s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/'  _site/ s3://gzip.savjee.be/
+echo '\n--> Syncing everything else'
+s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' $SITE_DIR $BUCKET
 
 # Sync: remaining files & delete removed
-s3cmd sync --acl-public --delete-removed  _site/ s3://gzip.savjee.be/
+s3cmd sync --acl-public --delete-removed $SITE_DIR $BUCKET

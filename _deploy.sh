8f50d8e11afc342aaa1443789f921cd292915855
Xavier Decuyper <xavier.decuyper@gmail.com>
2014-01-16 20:16:13 +0100

    Revamped _deploy script for automatic Gzip compression to S3

--- _deploy.sh
+++ _deploy.sh
@@ -1,30 +1,57 @@
+##
 # Configuration options
+##
 BUCKET='s3://gzip.savjee.be/'
 SITE_DIR='_site/'
 
-# Run Jekyll
-echo '-> Running Jekyll'
+##
+# Color stuff
+##
+NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+
+function red() {
+    echo "$RED$*$NORMAL"
+}
+
+function green() {
+    echo "$GREEN$*$NORMAL"
+}
+
+function yellow() {
+    echo "$YELLOW$*$NORMAL"
+}
+
+##
+# Actual script
+##
+red '--> Running Jekyll'
 Jekyll build
 
-# Upload to S3!
-echo '\n\n-> Uploading to S3'
 
+red '--> Gzipping all html, css and js files'
+find $SITE_DIR \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 {} \; -exec mv {}.gz {} \;
+
+
+yellow '--> Uploading css files'
+s3cmd sync --exclude '*.*' --include '*.css' --add-header='Content-Type: text/css' --add-header='Cache-Control: max-age=604800' --add-header='Content-Encoding: gzip' $SITE_DIR $BUCKET
+
+
+yellow '--> Uploading js files'
+s3cmd sync --exclude '*.*' --include '*.js' --add-header='Content-Type: application/javascript' --add-header='Cache-Control: max-age=604800' --add-header='Content-Encoding: gzip' $SITE_DIR $BUCKET
 
 # Sync media files first (Cache: expire in 10weeks)
-echo '\n--> Syncing media files...'
-s3cmd sync --acl-public --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header='Expires: Sat, 20 Nov 2020 18:46:39 GMT' --add-header='Cache-Control: max-age=6048000' $SITE_DIR $BUCKET
+yellow '--> Uploading images (jpg, png, ico)'
+s3cmd sync --exclude '*.*' --include '*.png' --include '*.jpg' --include '*.ico' --add-header='Expires: Sat, 20 Nov 2020 18:46:39 GMT' --add-header='Cache-Control: max-age=6048000' $SITE_DIR $BUCKET
 
-# Sync Javascript and CSS assets next (Cache: expire in 1 week)
-echo '\n--> Syncing .js and .css files...'
-s3cmd sync --acl-public --exclude '*.*' --include  '*.css' --include '*.js' --add-header='Cache-Control: max-age=604800' $SITE_DIR $BUCKET
 
 # Sync html files (Cache: 2 hours)
-echo '\n--> Syncing .html'
-s3cmd sync --acl-public --exclude '*.*' --include  '*.html' --add-header='Cache-Control: max-age=7200, must-revalidate' $SITE_DIR $BUCKET
+yellow '--> Uploading html files'
+s3cmd sync --exclude '*.*' --include '*.html' --add-header='Content-Type: text/html' --add-header='Cache-Control: max-age=7200, must-revalidate' --add-header="Content-Encoding: gzip" $SITE_DIR $BUCKET
 
-# Sync everything else, but ignore the assets!
-echo '\n--> Syncing everything else'
-s3cmd sync --acl-public --exclude '.DS_Store' --exclude 'assets/' $SITE_DIR $BUCKET
 
-# Sync: remaining files & delete removed
-s3cmd sync --acl-public --delete-removed $SITE_DIR $BUCKET
+# Sync everything else
+yellow '--> Syncing everything else'
+s3cmd sync --exclude '.DS_Store' --delete-removed $SITE_DIR $BUCKET

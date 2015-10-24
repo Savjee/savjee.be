0a129e10c231e798dce3abf9e123c5890135cf0b
Xavier Decuyper <xavier.decuyper@gmail.com>
2015-10-24 22:14:06 +0200

    New social buttons!

--- assets/js/StaticShareButtons.js
+++ assets/js/StaticShareButtons.js
@@ -1,33 +1,45 @@
 var StaticShareButtons = {
-	twitterButton: document.querySelector('.share-button-twitter'),
-	facebookButton: document.querySelector('.share-button-facebook'),
+    twitterButton: document.querySelector('.socialbutton.twitter'),
+    facebookButton: document.querySelector('.socialbutton.facebook'),
+    redditButton: document.querySelector('.socialbutton.reddit'),
 
-	init: function(){
-		this.injectScript('http://urls.api.twitter.com/1/urls/count.json?url=' + 
-			escape(this.twitterButton.dataset.shareUrl) + '&callback=' + 'StaticShareButtons.processTwitter');
+    init: function () {
+        this.injectScript('http://urls.api.twitter.com/1/urls/count.json?url=' +
+            encodeURI(this.twitterButton.dataset.shareUrl) + '&callback=' + 'StaticShareButtons.processTwitter');
 
-		this.injectScript('http://graph.facebook.com/?id='+ 
-			escape(this.facebookButton.dataset.shareUrl) +'&callback=StaticShareButtons.processFacebook');
-	},
+        this.injectScript('http://graph.facebook.com/?id=' +
+            encodeURI(this.facebookButton.dataset.shareUrl) + '&callback=StaticShareButtons.processFacebook');
 
-	injectScript: function(url){
-		var script = document.createElement('script');
-		script.async = true;
-		script.src = url;
-		document.body.appendChild(script);
-	},
+        this.injectScript('https://www.reddit.com/api/info.json?jsonp=StaticShareButtons.processReddit&url='
+            + encodeURI(this.facebookButton.dataset.shareUrl))
+    },
 
-	processTwitter: function(data){
-		if(data.count != undefined){
-			this.twitterButton.querySelector('.count').innerHTML = data.count;
-		}
-	},
+    injectScript: function (url) {
+        var script = document.createElement('script');
+        script.async = true;
+        script.src = url;
+        document.body.appendChild(script);
+    },
 
-	processFacebook: function(data){
-		if(data.shares != undefined){
-			this.facebookButton.querySelector('.count').innerHTML = data.shares;
-		}
-	}
-}
-	
+    processTwitter: function (data) {
+        if (data.count != undefined) {
+            this.twitterButton.querySelector('.shares').innerHTML = data.count;
+        }
+    },
+
+    processFacebook: function (data) {
+        if (data.shares != undefined) {
+            this.facebookButton.querySelector('.shares').innerHTML = data.shares;
+        }
+    },
+
+    processReddit: function (data) {
+        if(data.data != undefined){
+            if (data.data.children.length > 0) {
+                this.redditButton.querySelector('.shares').innerHTML = data.data.children[0].data.score;
+            }
+        }
+    }
+};
+
 StaticShareButtons.init();

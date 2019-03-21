73afa4bc67c7bf9b9b786324a7bf3303f16db69a
Xavier Decuyper <hi@savjee.be>
2019-03-21 09:20:08 +0100

    Lazy load Disqus comments

--- _assets/js/all.js
+++ _assets/js/all.js
@@ -1,2 +1,10 @@
 //= require external-links
 //= require lib/instantpage
+//= require lib/disqusLoader
+
+// Lazy load Disqus comments
+try{
+	disqusLoader('#disqus_thread', { scriptUrl: 'https://savjee.disqus.com/embed.js' });
+}catch(e){
+	console.error(e);
+}

34424619e8959a2e07f609d89b7ad4202aee9077
Xavier Decuyper <xavier.decuyper@gmail.com>
2020-02-24 15:29:22 +0100

    Enable Google Optimize for A/B test

--- _assets/js/analytics.js
+++ _assets/js/analytics.js
@@ -5,4 +5,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-XXXXXXXX-X', 'savjee.be');
+ga('require', 'GTM-MHSDRZ9');
 ga('send', 'pageview');

8ddbc6e2157bd31299331929167ab13b18428c3a
Xavier Decuyper <xavier.decuyper@gmail.com>
2021-02-08 09:57:14 +0100

    Load Prism themes straight from node_modules (syntax highlighting)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require(`tailwindcss`)(`./styles/tailwind.config.js`),
     require(`autoprefixer`),
     ...(process.env.NODE_ENV === "production"

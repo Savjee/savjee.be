7af12d00dcdcb73391a7efcb7dc99fcc0800336e
Xavier Decuyper <xavier.decuyper@gmail.com>
2021-02-08 13:45:10 +0100

    Be able to link to video pages + break build on broken link

--- .eleventy.js
+++ .eleventy.js
@@ -53,29 +53,24 @@ module.exports = function (eleventyConfig) {
   // Define a post_url Liquid tag for cross referencing
   // Original creator: https://rusingh.com/articles/2020/04/24/implement-jekyll-post-url-tag-11ty-shortcode/
   // Adapted by me to work with filename instead of slug.
-  const linkHandler = function(collection, filename){
-    try {
-      if (collection.length < 1) {
-        throw "Collection appears to be empty";
-      }
-      if (!Array.isArray(collection)) {
-        throw "Collection is an invalid type - it must be an array!";
-      }
-      if (typeof filename !== "string") {
-        throw "Filename is an invalid type - it must be a string!";
-      }
+  const linkHandler = function (collection, filename) {
+    if (collection.length < 1) {
+      throw "Collection appears to be empty";
+    }
+    if (!Array.isArray(collection)) {
+      throw "Collection is an invalid type - it must be an array!";
+    }
+    if (typeof filename !== "string") {
+      throw "Filename is an invalid type - it must be a string!";
+    }
 
-      const found = collection.find(p => p.template.parsed.base.replace('.md', '') == filename.replace('.md', ''));
-      if (found === 0 || found === undefined) {
-        throw `${filename} not found in specified collection.`;
-      } else {
-        return found.url;
-      }
-    } catch (e) {
-      console.error(
-        `An error occured while searching for the url to ${collection} ${filename}. Details:`,
-        e
-      );
+    const found = collection.find(p => p.template.inputPath.indexOf(filename) > -1);
+    if (found === 0 || found === undefined) {
+      // When nothing was found, throw an error to break the build.
+      // Broken links should not be allowed!
+      throw new Error(`File ${this.page.inputPath} wants to link to ${filename}, but it does not exist.`);
+    } else {
+      return found.url;
     }
   }
 
--- src/posts/2020/2020-01-14-four-years-on-youtube.md
+++ src/posts/2020/2020-01-14-four-years-on-youtube.md
@@ -22,7 +22,7 @@ Let's start with the raw statistics for 2019 and see how they compare to the yea
 
 Overall, 2019 was a year of much slower growth. I uploaded 35% fewer videos, and in return, I got 37% fewer views and 60% fewer new subscribers. That seems fair. Make fewer videos, get fewer views & subscribers in return. I can't complain about that.
 
-Part of the reason for this decline is still my choice of not making more videos related to cryptocurrencies. My [most popular video explains how Blockchains work]({% link collections.posts, '_videos/simply-explained/how-does-a-blockchain-work.md' %}), and that drew in a lot of new subscribers. After they got onto the channel, I started making fewer videos about cryptocurrencies which puts some people off (something I also addressed last year). Every new video I upload sees a period of subscriber decline before growing again.
+Part of the reason for this decline is still my choice of not making more videos related to cryptocurrencies. My [most popular video explains how Blockchains work]({% link collections.videos, 'simply-explained/how-does-a-blockchain-work.md' %}), and that drew in a lot of new subscribers. After they got onto the channel, I started making fewer videos about cryptocurrencies which puts some people off (something I also addressed last year). Every new video I upload sees a period of subscriber decline before growing again.
 
 Not much I can do about that. I want to keep distancing myself from cryptocurrencies and make it clear: I want to make videos about anything. If I find it interesting, I put it in a video. I'm not going to produce content to please my audience because that's not sustainable.
 

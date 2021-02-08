55c9aa64de5e8fedeeed06a43d399b7a5cf19f74
Xavier Decuyper <xavier.decuyper@gmail.com>
2021-02-08 13:21:31 +0100

    Added basic video page, showing all series + videos in them

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,24 @@ module.exports = function (eleventyConfig) {
     return markdownIt({ html: true }).render(content);
   });
 
+  /**
+   * Liquid filter: getVideosInSeries 
+   * 
+   * Usage in liquid:
+   *    {% assign vidsInSerie = collections.videos | getVideosInSeries: "Simply explained" %}
+   * 
+   * Takes the given video collection (parameter 1, before the pipe) 
+   * and returns only the videos in a given series (param 2, string).
+   */
+  eleventyConfig.addLiquidFilter("getVideosInSeries", function(collection, serieName) {
+    if(!Array.isArray(collection)){
+      throw new Error("getVideosInSeries: first parameter is not an array");
+    };
+    if(typeof serieName !== "string"){
+      throw new Error("getVideosInSeries: second parameter should be a string");
+    }
+    return collection.filter(m => m.data.series === serieName);
+  });
   
   eleventyConfig.addCollection('posts', (collectionApi) => {
     return collectionApi.getFilteredByGlob('src/posts/**/*.md')

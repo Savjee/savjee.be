de0227a45ea36aed938017601e68bb8efb5fd61a
Xavier Decuyper <xavier.decuyper@gmail.com>
2022-01-20 20:41:15 +0100

    Load WebP versions of YouTube thumbnails as well (overkill?)

--- src/utils/shortcode/xd_img.js
+++ src/utils/shortcode/xd_img.js
@@ -28,10 +28,34 @@ module.exports = function (originalHTML) {
     const imgSrc = matches[1];
 
     // Ignore remotely hosted images
-    if(imgSrc.startsWith("http")){
+    const isRemote = imgSrc.startsWith("http");
+    const isYouTubeThumbnail = imgSrc.indexOf("i.ytimg.com") !== -1;
+
+    if(isRemote && !isYouTubeThumbnail){
         return originalHTML;
     }
 
+    // YouTube thumbnails also have WebP versions. If we detect a YouTube URL in
+    // the form: i.ytimg.com/vi/VIDEO_ID/FORMAT.jpg, we extract the videoId and
+    // format and add the correct WebP version.
+    if(isYouTubeThumbnail){
+        const reg = /i\.ytimg\.com\/vi\/(.*)\/(.*)\.jpg/mi;
+        const matches = reg.exec(imgSrc);
+
+        if(matches === null) return originalHTML;
+
+        const videoId = matches[1];
+        const thumbnailSize = matches[2];
+
+        return `
+            <picture>
+                <source srcset="https://i.ytimg.com/vi_webp/${videoId}/${thumbnailSize}.webp" type="image/webp">
+                ${originalHTML}
+            </picture>
+        `;
+    }
+
+    // If we end up here, it must be a local image.
     const imgPath = path.join(__dirname, '/../../site/', imgSrc);
     const imgSrcWebP = imgSrc.substring(0, imgSrc.length - 3) + "webp";
     const imgPathWebP = imgPath.substring(0, imgPath.length - 3) + "webp";
@@ -41,8 +65,8 @@ module.exports = function (originalHTML) {
     statSync(imgPath, {throwIfNoEntry: true});
     statSync(imgPathWebP, {throwIfNoEntry: true});
 
-    // If we get here, the file exists. Wrap the original HTML inside a <picture>
-    // element.
+    // If we get here, the file exists. Wrap the original HTML inside a 
+    // <picture> element.
     return `
         <picture>
             <source srcset="${imgSrcWebP}" type="image/webp">

3602160ed186c17aee6623689f8f56095b1430cc
Xavier Decuyper <hi@savjee.be>
2022-06-18 11:13:16 +0200

    TOC: programatically show/hide it

--- src/site/_assets/js/table-of-contents.js
+++ src/site/_assets/js/table-of-contents.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-  if(document.body.classList.contains('toc_enabled')){
+  const classlist = document.body.classList;
+
+  // Check if classlist exists (old browsers don't have it), and if body has the
+  // class "toc_enabled".
+  if(classlist && classlist.contains('toc_enabled')){
     tocbot.init({
       // Where to render the table of contents.
       tocSelector: '.js-toc',
+
       // Where to grab the headings to build the table of contents.
       contentSelector: 'article',
+      
       // Which headings to grab inside of the contentSelector element.
+      // H1 is skipped, because it's used as the page title.
       headingSelector: 'h2, h3',
+      
       // For headings inside relative or absolute positioned containers within content.
       hasInnerContainers: true,
+
+      // Don't collapse any headings. By default, tocbot collapses everything,
+      // and only shows subitems when the parent is active.
       collapseDepth: 6,
     });
 
     // The block containing the table-of-contents is hidden by default. Make it
     // visible, once it has been rendered (and we know that JS has ran)
-    document.querySelector(".toc").style.display = "block";
+    document.querySelector(".toc").classList.add("visible");
   }
 });

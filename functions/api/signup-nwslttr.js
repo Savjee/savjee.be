1b59c5d9624cdd5e92c927a7f8e563840a5a225e
Xavier Decuyper <hi@savjee.be>
2022-08-06 22:26:19 +0200

    This should be working now

--- functions/api/signup-nwslttr.js
+++ functions/api/signup-nwslttr.js
@@ -1,27 +1,37 @@
 export async function onRequestPost({request, env}) {
-  var err = "";
-  try{
-    const url = "https://www.getrevue.co/api/v2/subscribers";
-    const token = env.REVUE_TOKEN;
-    const body = await request.json();
+  const url = "https://www.getrevue.co/api/v2/subscribers";
+  const token = env.REVUE_TOKEN;
+  const body = await request.json();
 
-    const response = await fetch(url,  {
-      body: JSON.stringify(body),
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-        'Authorization': 'Token ' + token,
-      },
-    });
-
-    err = await response.json();
-
+  if(!body.email){
     return new Response(JSON.stringify({
-      success: true,
-      error: null,
-    }));
-  }catch(e){
-    return new Response("Something went wrong: " + e + " " + JSON.stringify(err));
+      success: false,
+      error: "No email address provided",
+    }), { status: 400});
   }
-  
+
+  const response = await fetch(url,  {
+    body: JSON.stringify({
+      email: body.email,
+      double_opt_in: true,
+    }),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+      'Authorization': 'Token ' + token,
+    },
+  });
+
+  const res = await response.json();
+  if(res.error){
+    return new Response(JSON.stringify({
+      success: false,
+      error: res.error,
+    }), { status: 500});
+  }
+
+  return new Response(JSON.stringify({
+    success: true,
+    error: null,
+  }));
 }

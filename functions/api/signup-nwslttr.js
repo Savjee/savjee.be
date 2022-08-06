83f50622f71d09104f00a57f37855fbd61d62e8e
Xavier Decuyper <hi@savjee.be>
2022-08-06 22:12:16 +0200

    Try 5

--- functions/api/signup-nwslttr.js
+++ functions/api/signup-nwslttr.js
@@ -1,17 +1,15 @@
-export async function onRequestPost({request, env}) {
-  var err = "";
+export async function onRequestPost(request) {
   try{
-    const body = await request.json();
-
     const url = "https://www.getrevue.co/api/v2/subscribers";
-    const token = env.REVUE_TOKEN;
-
+    //const token = env.REVUE_TOKEN;
+    const body = await request.json();
+    
     const response = await fetch(url,  {
       body: JSON.stringify(body),
       method: 'POST',
       headers: {
         'content-type': 'application/json;charset=UTF-8',
-        'Authorization': 'Token ' + token,
+        'Authorization': 'Token ' + REVUE_TOKEN,
       },
     });
 

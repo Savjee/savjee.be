b4c746deefd1f950b6130c7059da029713937f4d
Xavier Decuyper <hi@savjee.be>
2022-08-07 20:52:52 +0200

    Figured out local testing + refactoring

---
+++ functions/newsletter/signup.js
@@ -0,0 +1,111 @@
+/**
+ * This function is called when a visitor signs up for the email newsletter.
+ * It receives the email address from the user, adds it to Revue, and redirects
+ * based on the response.
+ * 
+ * It runs as a Cloudflare Pages Function, which is based on their Workers
+ * platform. However, it does receive parameters differently and has different
+ * capabilities: https://developers.cloudflare.com/pages/platform/functions/
+ * 
+ * Redirects towards:
+ *   - /newsletter/signup/success
+ *     When subscriber was correctly added
+ * 
+ *   - /newsletter/signup/already
+ *     When email address is already on the list
+ * 
+ *   - /newsletter/signup/failed
+ *     When Revue API returned an error that couldn't be handled
+ */
+export async function onRequestPost({request, env})
+{
+  // Extract the domain from which this Function was called. This is needed for
+  // the redirect (which requires a full URL), and because I want to support
+  // Cloudflare Pages preview deployments (which get unique subdomains).
+  const url = new URL(request.url);
+  const baseUrl = `https://${url.host}/newsletter/signup`;
+
+  // Extract form data
+  const formBody = await request.formData().catch(_ => {
+    console.error('No form data provided');
+    return redirect(`${baseUrl}/failed`);
+  });
+  const { email } = Object.fromEntries(formBody);
+
+  if(!email){
+    console.error('No email address provided');
+    return redirect(`${baseUrl}/failed`);
+  }
+
+  // When running locally, don't make requests to Revue and simulate a
+  // successful registration. But strip the HTTPS, because that's not available.
+  if(url.host.indexOf('localhost') === 0){
+    const redirectUrl = `${baseUrl}/success`.replace('https', 'http');
+    return redirect(redirectUrl);
+  }
+
+  // Send the email address to Revue
+  const revueUrl = 'https://www.getrevue.co/api/v2/subscribers';
+  const revueToken = env.REVUE_TOKEN;
+
+  const req = await fetch(revueUrl, {
+    body: JSON.stringify({
+      email: email,
+      double_opt_in: true, // GDPR!
+    }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Authorization': `Token ${revueToken}`,
+    },
+  });
+
+  const res = await req.json();
+  if(res.error){
+    if(res.error.email 
+      && res.error.email[0] === 'This email address has already been confirmed'){
+      // TODO: try to resend the confirmation email
+      return redirect(`${baseUrl}/already`);
+    }
+
+    console.error("Received error from Revue:", JSON.stringify(res));
+    return redirect(`${baseUrl}/failed`);
+  }
+
+  await notify(email);
+  return redirect(`${baseUrl}/success`);
+}
+
+/**
+ * Little helper function that redirects the user with HTTP code 303:
+ * 
+ * HTTP 303 - See Other
+ * The server sent this response to direct the client to get the requested 
+ * resource at another URI with a GET request.
+ */
+function redirect(url){
+  return Response.redirect(url, 303);
+}
+
+function notify(email){
+  return fetch(new Request("https://api.mailchannels.net/tx/v1/send", {
+    "method": "POST",
+    "headers": {
+        "content-type": "application/json",
+    },
+    "body": JSON.stringify({
+        "personalizations": [
+          {"to": [ {"email": "hi@savjee.be", "name": "Xavier Decuyper"}]}
+        ],
+        "from": {
+          "email": "bot@savjee.be",
+          "name": "Savjee Website Bot",
+        },
+        "subject": "New subscriber!",
+        "content": [{
+          "type": "text/plain",
+          "value": `This email address tried signing up for the newsletter: ${email}`,
+        }],
+    }),
+  }));
+}

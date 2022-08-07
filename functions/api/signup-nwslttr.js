1db6cd76a72b22ff07149cc52f52386d764a6dd6
Xavier Decuyper <hi@savjee.be>
2022-08-07 08:26:06 +0200

    Catching errors to see what's going on now

--- functions/api/signup-nwslttr.js
+++ functions/api/signup-nwslttr.js
@@ -1,49 +1,53 @@
 export async function onRequestPost({request, env}) {
-  const url = "https://www.getrevue.co/api/v2/subscribers";
-  const token = env.REVUE_TOKEN;
-  const body = await request.json();
+  try{
+    const url = "https://www.getrevue.co/api/v2/subscribers";
+    const token = env.REVUE_TOKEN;
+    const body = await request.json();
 
-  if(!body.email){
-    return Response.redirect("/newsletter/signup/failed", 400);
+    if(!body.email){
+      return Response.redirect("/newsletter/signup/failed", 400);
 
-    return new Response(JSON.stringify({
-      success: false,
-      error: "No email address provided",
-    }), { status: 400});
-  }
-
-  const response = await fetch(url,  {
-    body: JSON.stringify({
-      email: body.email,
-      double_opt_in: true,
-    }),
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
-      'Authorization': 'Token ' + token,
-    },
-  });
-
-  const res = await response.json();
-  if(res.error){
-    if(res.error.email && res.error.email[0] === "This email address has already been confirmed"){
-      return Response.redirect("/newsletter/signup/already", 200);
-
-      return new Response(JSON.stringify({
-        success: true,
-        error: "Already subscribed",
-      }));
+      // return new Response(JSON.stringify({
+      //   success: false,
+      //   error: "No email address provided",
+      // }), { status: 400});
     }
 
-    return new Response(JSON.stringify({
-      success: false,
-      error: res.error,
-    }), { status: 500});
-  }
+    const response = await fetch(url,  {
+      body: JSON.stringify({
+        email: body.email,
+        double_opt_in: true,
+      }),
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+        'Authorization': 'Token ' + token,
+      },
+    });
 
-  return Response.redirect("/newsletter/signup/success", 200);
-  return new Response(JSON.stringify({
-    success: true,
-    error: null,
-  }));
+    const res = await response.json();
+    if(res.error){
+      if(res.error.email && res.error.email[0] === "This email address has already been confirmed"){
+        return Response.redirect("/newsletter/signup/already", 200);
+
+        // return new Response(JSON.stringify({
+        //   success: true,
+        //   error: "Already subscribed",
+        // }));
+      }
+
+      return new Response(JSON.stringify({
+        success: false,
+        error: res.error,
+      }), { status: 500});
+    }
+
+    return Response.redirect("/newsletter/signup/success", 200);
+    // return new Response(JSON.stringify({
+    //   success: true,
+    //   error: null,
+    // }));
+  }catch(e){
+    return new Response("Error: " + JSON.stringify(e));
+  }
 }

043398fddff3d40ba86da1aff3bb5ad62c6e3326
Xavier Decuyper <hi@savjee.be>
2023-04-07 13:40:07 +0200

    Adding tailwind + start of new default layout

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/site/_includes/**/*.html"
+  ],
+  theme: {
+    colors: {
+      'savjeered': '#E62643',
+      'savjeeblue': '#1E2546',
+      'savjeeblack': '#333333',
+    },
+  },
+  plugins: [],
+}
+

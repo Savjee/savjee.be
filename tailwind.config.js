44c5b3b4d56df8105002f3866a417818c2aa85d4
Xavier Decuyper <hi@savjee.be>
2023-04-08 08:27:09 +0200

    General tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "src/site/_includes/**/*.html"
   ],
   theme: {
+    fontFamily: {
+      sans: [
+        "Poppins", ...defaultTheme.fontFamily.sans
+      ]
+    },
     colors: {
       'savjeered': '#E62643',
       'savjeeblue': '#1E2546',
       'savjeeblack': '#333333',
+      'savjeelightgrey': '#E5E5E5',
     },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '32px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '4xl': ['60px', '84px'], // H1
+      '3xl': ['48px', '67px'], // H2
+      '2xl': ['36px', '44px'], // H3
+      'xl': ['24px', '44px'], // H4
+    }
   },
   plugins: [],
 }

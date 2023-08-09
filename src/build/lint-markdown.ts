06e9142132c68a23d75006242bf2bbfb747b0a5b
Xavier Decuyper <hi@savjee.be>
2023-08-09 15:40:16 +0200

    Add linter: no internal Obsidian links. Closes #54

--- src/build/lint-markdown.ts
+++ src/build/lint-markdown.ts
@@ -16,6 +16,7 @@ import {readFile} from "fs/promises";
         const fileErrors = await Promise.all([
             checkInternalLinks(path, content),
             checkVideoDuration(path, content),
+            checkObsidianLinks(path, content),
         ]);
 
         errors.push(...fileErrors);
@@ -61,5 +62,34 @@ async function checkVideoDuration(path: string, markdownContents: string): Promi
         return true;
     }
     
+    return false;
+}
+
+/**
+ * Make sure that Markdown files don't contain any internal Obsidian links.
+ * This would be the case when transfering my content from Obsidian to here.
+ */
+const regex_obsidian_link = /\[\[([^\[\]]+)\]\]/gmi;
+const internal_link_whitelist = [
+    "src/site/videos/mrna-vaccines-questions-and-misconceptions.md",
+    "src/site/posts/2014/2014-03-07-Jekyll-to-S3-deploy-script-with-gzip.md",
+    "src/site/posts/2020/2020-09-17-tuya-ir-hub-daikin-ac-home-assistant-esphome.md",
+    "src/site/posts/2021/2021-04-10-good-home-automation-should-be-boring.md",
+    "src/site/posts/2021/2021-04-18-migrating-this-blog-from-jekyll-to-eleventy.md",
+    "src/site/posts/2021/2021-07-06-filtering-spam-on-youtube-with-tensorflow-and-ai.md",
+    "src/site/posts/2022/2022-09-10-serverless-anagram-solver-with-cloudflare-r2-and-pages.md",
+]
+async function checkObsidianLinks(path: string, markdownContents: string): Promise<boolean> {
+    // Some files contain double brackets in code blocks, and should not be
+    // marked as wrong. Here, we implement a simple whitelist for those files.
+    if(internal_link_whitelist.indexOf(path) !== -1){
+        return false;
+    }
+
+    if(regex_obsidian_link.test(markdownContents)){
+        console.error("Error: file contains an internal Obsidian link", path);
+        return true;
+    }
+
     return false;
 }
